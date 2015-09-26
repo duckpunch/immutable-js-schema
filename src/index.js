0e3ee1f18488b5a42816c29b9d5e58042252abf4
Frank Lam <frank@duckpun.ch>
2015-09-26 01:54:01 -0700

    maybe it works..

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-import {includes} from 'lodash';
+import {chunk, some} from 'lodash';
+import {Map, List} from 'immutable';
 
 
-function MapSchema(key, value, ...) {
-    return function_that_validates_map;
+function checkPair(pair, key, value) {
+    const [key_check, value_check] = pair;
+    return key_check(key) && value_check(value);
 }
 
 
-function ListSchema(...functions_that_return_bool) {
-    return function_that_validates_list;
+export function MapSchema(...validators) {
+    const pairs = chunk(validators, 2);
+    if (validators.length % 2 === 1) {
+        throw 'Must have even length of validators';
+    }
+
+    function validatePair(value, key) {
+        return some(pairs, pair => checkPair(pair, key, value));
+    }
+
+    return function(candidate) {
+        if (Map.isMap(candidate)) {
+            return candidate.every(validatePair);
+        } else {
+            return false;
+        }
+    };
 }
 
 
-validate(
-    candidate,
-    MapSchema(
-        ListSchema(num, num),
-        s => includes([], s)
-    )
-)
+export function ListSchema(...validators) {
+    return function(candidate) {
+        if (List.isList(candidate) && candidate.size === validators.length) {
+            return candidate.every((value, index) => validators[index](value));
+        } else {
+            return false;
+        }
+    };
+}
