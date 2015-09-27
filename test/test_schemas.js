9d55cc524761dd46a60ca6f7fc237da0f60471c6
Frank Lam <frank@duckpun.ch>
2015-09-26 23:49:49 -0700

    Fixes #2 - good enough for now

--- test/test_schemas.js
+++ test/test_schemas.js
@@ -12,6 +12,12 @@ describe('ListSchema', function() {
     it('notices when strings arent numbers', function() {
         assert.ok(false === ListSchema(isNumber)(List.of('hi')));
     });
+
+    it('correctly tests order for 3 item lists', function() {
+        const schema = ListSchema(isString, isNumber, v => v === 'greetings');
+        const list = List.of('hi', 14.5, 'greetings');
+        assert.ok(schema(list));
+    });
 });
 
 
@@ -19,5 +25,16 @@ describe('MapSchema', function() {
     it('validates simple maps', function() {
         const schema = MapSchema(isString, isNumber);
         assert.ok(schema(Map({'hi': 5})));
+    });
+
+    it('still validates correctly when only some key-value validators match', function() {
+        const schema = MapSchema(
+            isString, isNumber,
+            v => isNumber(v) && (v % 2 === 0), isString
+        );
+
+        assert.ok(schema(
+            Map().set('hi', 5).set(14, 'roar')
+        ));
     });
 });
